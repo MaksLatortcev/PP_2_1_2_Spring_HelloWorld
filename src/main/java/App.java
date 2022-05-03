9915c9af538f4060022a78aae98d53e2e2c1d1a3
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-05-03 16:55:57 +0300

    add Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat oneCat = applicationContext.getBean(Cat.class);
+        Cat twoCat = applicationContext.getBean(Cat.class);
+
+        System.out.println(helloWorld1 == helloWorld2);
+        System.out.println(oneCat == twoCat);
     }
 }
